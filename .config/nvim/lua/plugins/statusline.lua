4d8f3392e5196a96ed5382ca4edd6fb9474e9209
pitahashi <yuki.ionic@gmail.com>
2026-07-23 19:00:42 +0900

    fix: statusline

--- .config/nvim/lua/plugins/statusline.lua
+++ .config/nvim/lua/plugins/statusline.lua
@@ -1,35 +1,7 @@
 return {
-	"nvim-lualine/lualine.nvim",
-	event = "VeryLazy",
-	opts = {
-		options = {
-			theme = "auto",
-			globalstatus = true,
-
-			section_separators = {
-				left = "",
-				right = "",
-			},
-			component_separators = {
-				left = "",
-				right = "",
-			},
-		},
-
-		sections = {
-			lualine_a = {
-				{
-					"mode",
-					fmt = function(str)
-						return str:sub(1, 1)
-					end,
-				},
-			},
-			lualine_b = { "branch", "diff" },
-			lualine_c = { "filename" },
-			lualine_x = { "diagnostics", "filetype" },
-			lualine_y = { "progress" },
-			lualine_z = { "location" },
-		},
-	},
+	"nvim-mini/mini.statusline",
+	version = false,
+	config = function()
+		require("mini.statusline").setup()
+	end,
 }
