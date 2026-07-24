9f4eec4836a510b7c482cd9b3a1bd4e6c892e6f1
pitahashi <yuki.ionic@gmail.com>
2026-07-24 13:50:21 +0900

    fix: conform

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -1,28 +1,45 @@
 return {
-  "stevearc/conform.nvim",
-  event = { "BufReadPre", "BufNewFile" },
-  opts = function()
-    local util = require("conform.util")
+	"stevearc/conform.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	opts = function()
+		local function has_prettier_config(bufnr)
+			local path = vim.api.nvim_buf_get_name(bufnr)
+			local dir = vim.fs.dirname(path)
 
-    local function js_formatter(bufnr)
-      if util.root_file({
-        ".prettierrc",
-        ".prettierrc.json",
-        "prettier.config.js",
-      })(bufnr) then
-        return { "prettier" }
-      end
+			return vim.fs.find({
+				".prettierrc",
+				".prettierrc.json",
+				".prettierrc.yaml",
+				".prettierrc.yml",
+				".prettierrc.js",
+				".prettierrc.cjs",
+				".prettierrc.mjs",
+				"prettier.config.js",
+				"prettier.config.cjs",
+				"prettier.config.mjs",
+				"prettier.config.ts",
+			}, {
+				path = dir,
+				upward = true,
+			})[1] ~= nil
+		end
 
-      return { "oxfmt" }
-    end
+		local function js_formatter(bufnr)
+			if has_prettier_config(bufnr) then
+				return { "prettier" }
+			end
 
-    return {
-      formatters_by_ft = {
-        javascript = js_formatter,
-        javascriptreact = js_formatter,
-        typescript = js_formatter,
-        typescriptreact = js_formatter,
-      },
-    }
-  end,
+			return { "oxfmt" }
+		end
+
+		return {
+			formatters_by_ft = {
+				lua = { "stylua" },
+				javascript = js_formatter,
+				javascriptreact = js_formatter,
+				typescript = js_formatter,
+				typescriptreact = js_formatter,
+			},
+		}
+	end,
 }
