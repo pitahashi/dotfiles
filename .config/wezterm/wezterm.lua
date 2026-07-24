b5c23a3c6bec36c4c412429505ea5650d6e346d7
pitahashi <yuki.ionic@gmail.com>
2026-07-24 19:08:48 +0900

    fix: wez

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,78 +1,88 @@
 local wezterm = require("wezterm")
-
 local config = wezterm.config_builder()
+
 config.automatically_reload_config = true
-
-config.font_size = 14
-config.line_height = 1
-config.font = wezterm.font_with_fallback({
-	{ family = "HackGen35 Console NF", weight = 500 },
-})
-
-config.adjust_window_size_when_changing_font_size = false
-config.cursor_blink_rate = 0 -- 点滅が気になるなら止める
+config.font = wezterm.font("HackGen Console NF")
+config.font_size = 15.0
 config.use_ime = true
+config.line_height = 1.0
+config.window_background_opacity = 0.85
+config.macos_window_background_blur = 20
 config.color_scheme = "Pastel White (terminal.sexy)"
--- config.window_background_opacity = 0.80
--- config.macos_window_background_blur = 10
-config.window_decorations = "RESIZE"
-config.hide_tab_bar_if_only_one_tab = true
-config.tab_bar_at_bottom = false
-config.use_fancy_tab_bar = false
 
-config.initial_cols = 90
-config.initial_rows = 25
+----------------------------------------------------
+-- Tab
+----------------------------------------------------
+-- タイトルバーを非表示
+config.window_decorations = "RESIZE"
+-- タブバーの表示
+config.show_tabs_in_tab_bar = true
+-- タブが一つの時は非表示
+config.hide_tab_bar_if_only_one_tab = true
+-- falseにするとタブバーの透過が効かなくなる
+
+config.use_fancy_tab_bar = true
+
+-- タブバーの透過
+config.window_frame = {
+	inactive_titlebar_bg = "none",
+	active_titlebar_bg = "none",
+}
+
+-- タブバーを背景色に合わせる
+config.window_background_gradient = {
+	colors = { "#000000" },
+}
+
+-- タブの追加ボタンを非表示
+config.show_new_tab_button_in_tab_bar = false
+-- nightlyのみ使用可能
+-- タブの閉じるボタンを非表示
+-- config.show_close_tab_button_in_tabs = false
+config.show_close_tab_button_in_tabs = false
+
+-- タブ同士の境界線を非表示
 config.colors = {
 	tab_bar = {
-		background = "none",
 		inactive_tab_edge = "none",
 	},
 }
 
-config.window_frame = {
-	inactive_titlebar_bg = "none",
-	active_titlebar_bg = "none",
-	border_left_width = "2px",
-}
+-- タブの形をカスタマイズ
+-- タブの左側の装飾
+local SOLID_LEFT_ARROW = wezterm.nerdfonts.ple_lower_right_triangle
+-- タブの右側の装飾
+local SOLID_RIGHT_ARROW = wezterm.nerdfonts.ple_upper_left_triangle
 
-config.window_background_gradient = {
-	colors = {
-		"#191919",
-	},
-}
-
-config.show_new_tab_button_in_tab_bar = false
-
-config.window_padding = {
-	left = 10,
-	right = 10,
-	top = 8,
-	bottom = 8,
-}
-
-config.keys = {
-	{ key = "DownArrow", mods = "SHIFT|SUPER", action = wezterm.action.ScrollToBottom },
-	{ key = "UpArrow", mods = "SHIFT|SUPER", action = wezterm.action.ScrollToTop },
-}
-
-wezterm.on("format-tab-title", function(tab)
+wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
+	local background = "#5c6d74"
+	local foreground = "#FFFFFF"
+	local edge_background = "none"
 	if tab.is_active then
-		return {
-			{ Foreground = { Color = "#ffffff" } },
-			{ Text = "  " .. tab.active_pane.title .. "  " },
-		}
+		background = "#ae8b2d"
+		foreground = "#FFFFFF"
 	end
-
+	local edge_foreground = background
+	local title = "   " .. wezterm.truncate_right(tab.active_pane.title, max_width - 1) .. "   "
 	return {
-		{ Foreground = { Color = "#666666" } },
-		{ Text = "  " .. tab.active_pane.title .. "  " },
+		{ Background = { Color = edge_background } },
+		{ Foreground = { Color = edge_foreground } },
+		{ Text = SOLID_LEFT_ARROW },
+		{ Background = { Color = background } },
+		{ Foreground = { Color = foreground } },
+		{ Text = title },
+		{ Background = { Color = edge_background } },
+		{ Foreground = { Color = edge_foreground } },
+		{ Text = SOLID_RIGHT_ARROW },
 	}
 end)
 
-local keybinds = require("keybinds")
-config.keys = keybinds.keys
-config.key_tables = keybinds.key_tables
-
-config.leader = { key = "b", mods = "CTRL", timeout_milliseconds = 2000 }
+----------------------------------------------------
+-- keybinds
+----------------------------------------------------
+config.disable_default_key_bindings = true
+config.keys = require("keybinds").keys
+config.key_tables = require("keybinds").key_tables
+config.leader = { key = "q", mods = "CTRL", timeout_milliseconds = 2000 }
 
 return config
