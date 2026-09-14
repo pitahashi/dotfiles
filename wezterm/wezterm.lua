f59eb25c85928ac86377655698b18f3aedf9472b
pita <132864895+pitahashi@users.noreply.github.com>
2026-09-14 15:41:51 +0900

    Change leader key from 'b' to 'q' in wezterm config

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -77,6 +77,6 @@ end)
 config.disable_default_key_bindings = true
 config.keys = require("keybinds").keys
 config.key_tables = require("keybinds").key_tables
-config.leader = { key = "b", mods = "CTRL", timeout_milliseconds = 2000 }
+config.leader = { key = "q", mods = "CTRL", timeout_milliseconds = 2000 }
 
 return config
